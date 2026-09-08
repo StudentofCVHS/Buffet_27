4c677b39d3228333eaec7e50b60283a8065abf0d
StudentofCVHS <Nikofisher14@gmail.com>
2026-09-08 18:09:32 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+	 	String Happybday="Happy Birthday";
+		String Toyou=" to you.";
+		String Dearname=" dear proper name of somebody in the class";
+		System.out.println(Happybday+Toyou);
+		System.out.println(Happybday+Toyou);
+		System.out.println(Happybday+Dearname);
+		System.out.println(Happybday+Toyou);
 	}
 }
